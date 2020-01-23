bda6ca02c38a50c63b3fee515e40f528e6510008
justcris <cristian.scapin654@icloud.com>
2020-01-23 19:04:39 +0100

    0.1

---
+++ Fish.cs
@@ -0,0 +1,41 @@
+namespace fish_project {
+    public class Fish {
+        private string _name;    //GetName
+        private double _weight;  //GetWeight
+        private string _color;   //GetColor
+        private int    _speed;   //GetSpeed
+        private bool   _habitat; //true se è di mare, false se è di fiume o lago      //isSea - isSoft
+        private char   _style;   //GetStyle
+
+        public Fish(string name, double weight, string color, int speed, bool habitat) {
+            _style   = '.';
+            _name    = name;
+            _weight  = weight;
+            _color   = color;
+            _speed   = speed;
+            _habitat = habitat;
+        }
+
+        public Fish() : this("def_n", 0, "def_c", 0, false) {
+        }
+
+        public override string ToString() {
+            return "Nome : "     + this._name                                + "\n" +
+                   "Peso : "     + this._weight                              + "\n" +
+                   "Colore : "   + this._color                               + "\n" +
+                   "Velocità : " + this._speed                               + "\n" +
+                   "Habitat : "  + (this._habitat ? "Mare" : "Fiume - Lago") + "\n" +
+                   "Stile"       + this._style                               + "\n";
+        }
+
+        public bool isSea  => _habitat ? true : false;
+        public bool isSoft => _habitat ? false : true;
+
+        public string GetName   => _name;
+        public double GetWeight => _weight;
+        public string GetColor  => _color;
+        public int    GetSpeed  => _speed;
+        public char   GetStyle  => _style;
+        public string GetSwim   => "";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace fish_project {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
